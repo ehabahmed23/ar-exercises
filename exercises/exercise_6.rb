a9bf82b2cc58de0dae45865510c85792a9f3093b
Ehab Ahmed <ehabahmed2456@gmail.com>
2023-12-19 00:33:16 -0500

    Exercise 6: One-to-many association created employees and assigned them stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Zak", last_name: "Issa", hourly_rate: 60)
+@store1.employees.create(first_name: "Ehab", last_name: "Ahmed", hourly_rate: 60)
+@store1.employees.create(first_name: "Rou", last_name: "Lucky", hourly_rate: 30)
+@store1.employees.create(first_name: "Shanks", last_name: "Garling", hourly_rate: 55)
+
+@store2.employees.create(first_name: "Yassop", last_name: "King", hourly_rate: 350)
+@store2.employees.create(first_name: "Sanji", last_name: "Vinsmoke", hourly_rate: 400)
+@store2.employees.create(first_name: "Franky", last_name: "Cola", hourly_rate: 90)
+@store2.employees.create(first_name: "Tony", last_name: "Chopper", hourly_rate: 50)
