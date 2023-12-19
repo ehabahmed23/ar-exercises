1a9c3a38b79e84819d798ba7a9f0f63de853ed2e
Ehab Ahmed <ehabahmed2456@gmail.com>
2023-12-18 23:18:36 -0500

    Exercise 2: Update the first store also fixed Exercise 1 with saves

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,7 +5,12 @@ puts "----------"
 
 # Your code goes below here ...
 burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
-richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
-gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+burnaby.save
 
-puts Store.count
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+richmond.save
+
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+gastown.save
+
+p "Store count = #{Store.count}"
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -7,4 +7,5 @@ puts "----------"
 # Your code goes here ...
 @store1 = Store.find_by(id: 1)
 @store2 = Store.find_by(id: 2)
-@store1.name = "Mississauga" 
+@store1.name = "Mississauga"
+@store1.save
