786d0abe7aae47cba475c860ca5862993f8177b1
Ehab Ahmed <ehabahmed2456@gmail.com>
2023-12-19 00:42:14 -0500

    Exercise 7: Validations for both models Added validations to two models to enforce business rules

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,14 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+#  person.errors.full_messages # => ["name cannot be nil"]
+puts "What is the new store name?"
+
+@answer = gets.chomp
+@new_store = Store.create(name: @answer)
+
+if @new_store.errors.any?
+  @new_store.errors.full_messages.each do |error|
+    puts "Error! #{error}"
+  end
+end
